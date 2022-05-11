b44df096fe318e8978c67c24d84bff7bb67462cd
JLuedeking <jluedeking@gmail.com>
2022-05-10 18:26:50 -0600

    [FE] Luedecraig.com
    Finished updating homepage blurbs, removed hamburger menu for now on tablet/mobile, adjusted stylings for rsvp grid.

--- luedecraig_com/rsvp.php
+++ luedecraig_com/rsvp.php
@@ -1,14 +1,12 @@
 <?php 
-if(isset($_POST['submit'])){
     $to = "luedecraig@gmail.com";
     $from = "rsvp@luedecraig.com";
     $name = $_POST['name'];
+    $attending = $_POST['attending'];
     $subject = $name . " - RSVP Form Submission";
     $message = $name . " submitted their RSVP for the Luedecraig wedding. They marked themselves as " . $attending . ", and have requested " . $_POST['seats'] . " seats." . "\n\n" . "Dietary Restricions:\n" . $_POST['food'] . "\n\n" . "Song Recommendations:\n" . $_POST['songs'];
 
     $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     header('Location: faq.html');
-    }
 ?>
